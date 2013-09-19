66646b08e504d425f21b711b6f5bc9a29c88a708
Andrew Gustafson <gus4000@gmail.com>
2013-09-19 22:32:03 +0100

    Added specs2 dependency

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,11 @@ import sbt._
 import sbt.Keys._
 import sbtrelease.ReleasePlugin._
 
-object ScalaxmlBuild extends Build {
+object ScalaXmlBuild extends Build {
+
+  object Dependencies {
+    val specs2 = "org.specs2" %% "specs2" % "2.2.2"
+  }
 
   lazy val scalaxml = Project(
     id = "scala-xml",
@@ -10,7 +14,8 @@ object ScalaxmlBuild extends Build {
     settings = Project.defaultSettings ++ releaseSettings ++ Seq(
       organization := "agustafson",
       name := "scala-xml",
-      scalaVersion := "2.10.2"
+      scalaVersion := "2.10.2",
+      libraryDependencies += Dependencies.specs2
     )
   )
 }
