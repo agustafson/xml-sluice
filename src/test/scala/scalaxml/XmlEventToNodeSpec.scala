ee59d054ab5517e9b9d3726a919ec222d2c964e2
Andrew Gustafson <gus4000@gmail.com>
2013-09-21 20:27:27 +0100

    Handle child nodes

--- src/test/scala/scalaxml/XmlEventToNodeSpec.scala
+++ src/test/scala/scalaxml/XmlEventToNodeSpec.scala
@@ -1,5 +1,6 @@
 package scalaxml
 
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 import scala.xml._
 import scala.xml.pull._
@@ -8,19 +9,40 @@ import org.specs2.mutable.Specification
 class XmlEventToNodeSpec extends Specification {
   val simpleFlatNode = <test/>
   val singleElementNode = <test>hi</test>
+  val singleNested =
+    <root>
+      <parent role="father">
+        <name>Gus</name>
+        <child role="daughter">Elsa</child>
+      </parent>
+    </root>
 
   "XmlEventToNode" should {
     "convert a flat node" in {
       implicit val reader = createReader(simpleFlatNode)
-      val Some(node) = convertEventToNode
+      val node = convertEventToNode.head
       node.label === "test"
     }
 
     "convert a simple node" in {
       implicit val reader = createReader(singleElementNode)
-      val Some(node) = convertEventToNode
+      val node = convertEventToNode.head
       node.label === "test"
       node.text === "hi"
+    }
+
+    "convert a single nested node" in {
+      implicit val reader = createReader(singleNested)
+
+      val nodes = convertEventToNode
+      val parentNode = nodes \ "parent"
+      (parentNode \ "@role").text === "father"
+      (parentNode \ "name").text === "Gus"
+      val childNode = parentNode \ "child"
+      (childNode \ "@role").text === "daughter"
+      childNode.text === "Elsa"
+
+      true === true
     }
   }
 
@@ -28,26 +50,28 @@ class XmlEventToNodeSpec extends Specification {
     new XMLEventReader(Source.fromString(xml.toString))
   }
 
-  def convertEventToNode(implicit reader: XMLEventReader): Option[Node] = {
-    var node: Option[Node] = None
-    while (reader.hasNext) {
-      reader.next match {
-        case EvElemStart(prefix, label, attrs, scope) =>
-          node = Some(new Elem(prefix, label, attrs, scope, true))
-        case EvText(text) =>
-          val textNode = Text(text)
-          node = node.map(elem => new Elem(elem.prefix, elem.label, elem.attributes, elem.scope, true, (elem.child :+ textNode):_*))
-        case EvElemEnd(prefix, label) =>
-          node match {
-            case None =>
-              throw new IllegalStateException(s"Attempting to close element $prefix:$label but current element has not been set")
-            case Some(elem) if prefix != elem.prefix || label != elem.label =>
-              throw new IllegalStateException(s"Current element ${elem.prefix}:${elem.label} had closing element $prefix:$label")
-            case _ =>
-              return node
-          }
+
+  def convertEventToNode(implicit reader: XMLEventReader): Seq[Node] = {
+    def convert(parent: Option[Elem]): Seq[Node] = {
+      val nodes = ListBuffer[Node]()
+      while (reader.hasNext) {
+        reader.next match {
+          case EvElemStart(prefix, label, attrs, scope) =>
+            nodes ++= convert(Some(new Elem(prefix, label, attrs, scope, true)))
+          case EvText(text) =>
+            println(s"parent: $parent; text: $text")
+            nodes += Text(text)
+          case EvElemEnd(prefix, label) =>
+            parent map { parentElem =>
+              println(s"parent: $parent; nodes: $nodes")
+              if (prefix != parentElem.prefix || label != parentElem.label)
+                throw new IllegalStateException(s"Current element ${parentElem.prefix}:${parentElem.label} had closing element $prefix:$label")
+              return parentElem.copy(child = parentElem.child ++ nodes.toSeq)
+            } getOrElse { throw new IllegalStateException("Current parent is not set") }
+        }
       }
+      nodes.toSeq
     }
-    node
+    convert(None)
   }
 }
