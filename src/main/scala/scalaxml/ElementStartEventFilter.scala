1e3b8f7dd470f15fe61a75bcf7e4510dadca78aa
Andrew Gustafson <gus4000@gmail.com>
2013-09-22 10:48:56 +0100

    Handle depth level filter

---
+++ src/main/scala/scalaxml/ElementStartEventFilter.scala
@@ -0,0 +1,16 @@
+package scalaxml
+
+import scala.xml.pull._
+
+trait ElementStartEventFilter { self: XmlNodeReader =>
+  def includeNode: EvElemStart => Boolean
+}
+
+trait IncludeAllElementStartEventFilter extends ElementStartEventFilter { self: XmlNodeReader =>
+  def includeNode: (EvElemStart) => Boolean = _ => true
+}
+
+trait DepthBasedElementStartEventFilter extends ElementStartEventFilter { self: XmlNodeReader =>
+  def depth: Int
+  def includeNode: (EvElemStart) => Boolean = _ => currentDepth == depth
+}
--- src/main/scala/scalaxml/XmlNodeReader.scala
+++ src/main/scala/scalaxml/XmlNodeReader.scala
@@ -1,33 +1,66 @@
 package scalaxml
 
-import scala.collection.mutable.ListBuffer
 import scala.xml._
 import scala.xml.pull._
 
-class XmlNodeReader(reader: XMLEventReader) {
+class XmlNodeReader(reader: XMLEventReader) { self: ElementStartEventFilter =>
+  private[scalaxml] var currentDepth = 0
+  private var startingNodeDepth: Option[Int] = None
 
   def readNodes: Seq[Node] = {
-    def convertXmlEventsToNodes(parent: Option[Elem]): Seq[Node] = {
-      val nodes = ListBuffer[Node]()
+    def convertXmlEventsToNodes(parent: Option[Elem], children: Seq[Node]): Stream[Node] = {
       while (reader.hasNext) {
         reader.next match {
-          case EvElemStart(prefix, label, attrs, scope) =>
-            nodes ++= convertXmlEventsToNodes(Some(new Elem(prefix, label, attrs, scope, true)))
+
+          case event @ EvElemStart(prefix, label, attrs, scope) =>
+            val shouldIncludeNode = includeNode(event)
+            startingNodeDepth = startingNodeDepth orElse { if (shouldIncludeNode) Some(currentDepth) else None }
+            println(s"START $label currentDepth: $currentDepth; startingNodeDepth: $startingNodeDepth; shouldIncludeNode: $shouldIncludeNode")
+            currentDepth = currentDepth + 1
+            val (newParent,newChildren) = startingNodeDepth match {
+              case Some(startingDepth) if currentDepth >= startingDepth =>
+                (Some(new Elem(prefix, label, attrs, scope, true)), Nil)
+              case _ =>
+                (parent, children)
+            }
+            return convertXmlEventsToNodes(parent = newParent, children = newChildren)
+
           case EvText(text) =>
-            println(s"parent: $parent; text: $text")
-            nodes += Text(text)
+            val newChildren = startingNodeDepth match {
+              case Some(startingDepth) if currentDepth >= startingDepth =>
+                children :+ Text(text)
+              case _ =>
+                children
+            }
+            return convertXmlEventsToNodes(parent, newChildren)
+
           case EvElemEnd(prefix, label) =>
-            parent map { parentElem =>
-              println(s"parent: $parent; nodes: $nodes")
-              if (prefix != parentElem.prefix || label != parentElem.label)
-                throw new IllegalStateException(s"Current element ${parentElem.prefix}:${parentElem.label} had closing element $prefix:$label")
-              return parentElem.copy(child = parentElem.child ++ nodes.toSeq)
-            } getOrElse { throw new IllegalStateException("Current parent is not set") }
+            currentDepth = currentDepth - 1
+            println(s"END $label currentDepth: $currentDepth; startingNodeDepth: $startingNodeDepth")
+            startingNodeDepth match {
+              case Some(startingDepth) if currentDepth == startingDepth =>
+                startingNodeDepth = None
+                parent map { parentElem =>
+                  println(s"parent: $parent; children: $children")
+                  if (prefix != parentElem.prefix || label != parentElem.label)
+                    throw new IllegalStateException(s"Current element ${parentElem.prefix}:${parentElem.label} had closing element $prefix:$label")
+                  val newElem = parentElem.copy(child = parentElem.child ++ children)
+                  println(s"Returning new element as head of stream: $newElem")
+                  return newElem #:: convertXmlEventsToNodes(parent = None, children = Nil)
+                } getOrElse { throw new IllegalStateException("No parent set") }
+              case _ =>
+                return convertXmlEventsToNodes(parent, children)
+            }
+
+          case _ =>
+            return convertXmlEventsToNodes(parent, children)
+
         }
       }
-      nodes.toSeq
+      println(s"Returning result: empty stream with parent: $parent; children: $children")
+      Stream.empty
     }
-    convertXmlEventsToNodes(None)
+    convertXmlEventsToNodes(parent = None, children = Nil)
   }
 
 }
