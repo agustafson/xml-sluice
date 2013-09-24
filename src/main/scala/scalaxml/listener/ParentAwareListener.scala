75e82bcb65bf9f378cb5672feeea61c2b29a54f5
Andrew Gustafson <gus4000@gmail.com>
2013-09-24 23:47:05 +0100

    Cleanup ParentAwareListener

--- src/main/scala/scalaxml/listener/ParentAwareListener.scala
+++ src/main/scala/scalaxml/listener/ParentAwareListener.scala
@@ -14,32 +14,17 @@ trait ParentAwareListener extends XmlEventListener with DepthAwareListener {
   override def preProcessing: (XMLEvent) => Unit = { event =>
     super.preProcessing(event)
     event match {
-      /*case EvElemEnd(_,label) =>
-        if (currentDepth <= parents.length)
-          parents.pop()
-        println(s"END $label; parents: $parents")*/
-      case EvElemStart(_,label,_,_) =>
-        println(s"start stuff $label")
       case EvElemEnd(_,label) =>
-        println(s"END $label; parents: $parents; currentDepth: $currentDepth")
-        //if (currentDepth < parents.length)
-          parents.pop()
+        parents.pop()
       case _ =>
     }
   }
 
   override def postProcessing: (XMLEvent, Option[Node]) => Unit = { case (event,nodeResult) =>
-    //super.postProcessing.apply(event,nodeResult)
+    super.postProcessing(event, nodeResult)
     event match {
       case EvElemStart(_,label,_,_) =>
-        currentDepth = currentDepth + 1
-        println(s"START $label; parents: $parents; currentDepth: $currentDepth")
-        if (currentDepth > parents.length)
-          parents.push(label)
-      /*case (EvElemStart(_,label,_,_), _) =>
-        if (currentDepth == parents.length)
-          parents.push(label)
-        println(s"START $label; parents: $parents")*/
+        parents.push(label)
       case _ =>
     }
   }
